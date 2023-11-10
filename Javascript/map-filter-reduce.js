045df8711e535892ce7d6f65869f29f31b3f87e4
Subhadip Das <subhadipdas911@gmail.com>
2023-11-10 22:46:45 +0530

    Electronics department total price

--- Javascript/map-filter-reduce.js
+++ Javascript/map-filter-reduce.js
@@ -1,42 +1,59 @@
-// ! Age Difference between the Youngest and the Oldest
-
-const input = [
-      {
-        name: "John",
-        age: 13,
-      },
-      {
-        name: "Mark",
-        age: 56,
-      },
-      {
-        name: "Rachel",
-        age: 45,
-      },
-      {
-        name: "Nate",
-        age: 67,
-      },
-      {
-        name: "Jennifer",
-        age: 65,
-      },
+const products = [
+      { name: "Product 1", price: 20, category: "Electronics" },
+      { name: "Product 2", price: 30, category: "Clothes" },
+      { name: "Product 3", price: 40, category: "Electronics" },
+      { name: "Product 4", price: 50, category: "Clothes" },
+      { name: "Product 5", price: 60, category: "Clothes" },
+      { name: "Product 6", price: 70, category: "Electronics" },
+      { name: "Product 7", price: 80, category: "Clothes" },
+      { name: "Product 8", price: 90, category: "Electronics" },
     ];
 
 
+// const output = products.reduce(function (acc,curr){
+//       if (curr.category == 'Electronics'){
+//             acc.push(curr.name);
+//       }
+// },[])
 
-let outputYoungest = input.reduce(function (acc,curr){
-      if (curr.age < acc){
-            acc = curr.age;
-      }
-      return acc;
-},100)
+// function f(ele){
+//       if (ele.category == "Electronics"){
+//             return true
+//       }else{
+//             return false;
+//       }
+// }
 
-let outputOldest = input.reduce(function (acc,curr){
-      if (curr.age > acc){
-            acc = curr.age;
+// function f1(ele){
+//       return ele.name;
+// }
+
+// const output = products.filter(f).map(f1);
+
+// console.log(output);
+
+
+// const output = products.reduce(function (acc,curr){
+//       if (curr.category=="Electronics"){
+//             acc.push(curr.name);
+//       }
+//       return acc;
+// },[])
+
+// console.log(output);
+
+function f(ele){
+      if (ele.category == "Electronics"){
+            return true;
+      }else{
+            return false;
       }
+}
+
+
+const output = products.filter(f).reduce(function (acc,curr){
+      acc = acc + curr.price;
       return acc;
 },0)
 
-console.log(outputOldest - outputYoungest);
+console.log(output);
