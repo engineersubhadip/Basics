fcd5379679c0d1e6073f889b400d9b3b3be39caf
Subhadip Das <subhadipdas911@gmail.com>
2023-11-10 20:30:49 +0530

    filter with reduce

--- Javascript/map-filter-reduce.js
+++ Javascript/map-filter-reduce.js
@@ -1,8 +1,12 @@
 function f(ele){
-      return ele * ele;
+      return ele > 0;
 }
 
-const input = [1, 2, 3, 4, 5];
+const input = [1, -4, 12, 0, -3, 29, -150];
 
-const output = input.map(f);
+const output = input.filter(f).reduce(function(acc,curr){
+      acc = acc + curr;
+      return acc;
+},0)
+
 console.log(output);
