a171defd14d2d5a9ccb03f34d09494c1980d3b77
Subhadip Das <subhadipdas911@gmail.com>
2023-11-11 19:04:40 +0530

    HR VS IT DEPARTMENT

--- Javascript/map-filter-reduce.js
+++ Javascript/map-filter-reduce.js
@@ -1,54 +1,64 @@
-// !return an array of objects containing only the categories that have an average price above 50.
-
-const products = [
-      { name: "Product 1", price: 20, category: "Electronics" },
-      { name: "Product 2", price: 30, category: "Clothes" },
-      { name: "Product 3", price: 40, category: "Electronics" },
-      { name: "Product 4", price: 50, category: "Clothes" },
-      { name: "Product 5", price: 60, category: "Clothes" },
-      { name: "Product 6", price: 70, category: "Electronics" },
-      { name: "Product 7", price: 80, category: "Clothes" },
-      { name: "Product 8", price: 90, category: "Electronics" },
+//! return an array of objects containing only the departments that have an average salary above 65000.
+const employees = [
+      { name: "John", salary: 50000, department: "IT" },
+      { name: "Jane", salary: 60000, department: "HR" },
+      { name: "Bob", salary: 55000, department: "IT" },
+      { name: "Sophie", salary: 75000, department: "HR" },
+      { name: "Mike", salary: 65000, department: "IT" },
+      { name: "Emily", salary: 80000, department: "HR" },
+      { name: "David", salary: 70000, department: "IT" },
     ];
 
-// * Target 1: {'Electronics':[10,20,30], 'Clothes':[30,40,15]}
+// * Target One:- {"IT":[10,20,30]; "HR":[12,13,19]}
 
-const tarOne = products.reduce(function(acc,curr){
-      let currentCategory = curr.category;
-      let currPrice = curr.price;
+/**
+ * Over here we will list all the salaries for every department
+ */
 
-      if (!acc[currentCategory]){
-            acc[currentCategory] = [currPrice];
+const departmentSalaries = employees.reduce(function(acc,curr){
+      let currentDepartment = curr.department;
+      let currentSalary = curr.salary;
+
+      if (!acc[currentDepartment]){
+            acc[currentDepartment] = [currentSalary];
       }else{
-            acc[currentCategory].push(currPrice);
+            acc[currentDepartment].push(currentSalary);
       }
 
       return acc;
 },{})
 
-// * Target Two:- {'Electronics':52, 'Clothes':55}
+//  * Target Two:- {"IT" : 20, "HR": 30}
 
-const tarTwo = Object.keys(tarOne).reduce(function(acc,curr){
-      let currentCategory = curr;
-      let currentPrice = tarOne[curr].reduce(function(sum,price){
-            sum = sum + price;
-            return sum;
+/**
+ * At this section we will find out averages of all the departments
+ */
+
+const departmentAverage = Object.keys(departmentSalaries).reduce(function(acc,curr){
+      let currentDepartment = curr;
+      let currentSalary = departmentSalaries[curr].reduce(function(total,current){
+            total = total + current;
+            return total;
       },0)
+      let currentAverage = Math.ceil(currentSalary/departmentSalaries[curr].length);
 
-      acc[currentCategory] = currentPrice/tarOne[curr].length;
+      acc[currentDepartment] = currentAverage;
       return acc;
 },{})
 
-// * Target Three:- The Final Touch
+// * Target Three:- [{department:"HR", average:71667}] :-
+/**
+ * At this section we will filter out the department with the highest average
+ */
 
-const tarThree = Object.keys(tarTwo).reduce(function(acc,curr){
-      let currentCategory = curr;
-      let currentAverage = tarTwo[currentCategory];
+const highestAverageDepartment = Object.keys(departmentAverage).reduce(function(acc,curr){
+      let currentDepartment = curr;
+      let currentAverage = departmentAverage[curr];
 
-      if (currentAverage > 50){
-            acc.push({'category':currentCategory,'average':currentAverage});
+      if (currentAverage > 65000){
+            acc.push({'department':currentDepartment,'average':currentAverage});
       }
       return acc;
 },[])
 
-console.log(tarThree);
+console.log(highestAverageDepartment);
