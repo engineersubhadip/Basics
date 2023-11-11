79fbcc0e395948286ab5dce5e186bdadd003d52c
Subhadip Das <subhadipdas911@gmail.com>
2023-11-11 12:26:09 +0530

    return an array objects students who avg score>90

--- Javascript/map-filter-reduce.js
+++ Javascript/map-filter-reduce.js
@@ -1,70 +1,42 @@
-const products = [
-      { name: "Product 1", price: 20, category: "Electronics" },
-      { name: "Product 2", price: 30, category: "Clothes" },
-      { name: "Product 3", price: 40, category: "Electronics" },
-      { name: "Product 4", price: 50, category: "Clothes" },
-      { name: "Product 5", price: 60, category: "Clothes" },
-      { name: "Product 6", price: 70, category: "Electronics" },
-      { name: "Product 7", price: 80, category: "Clothes" },
-      { name: "Product 8", price: 90, category: "Electronics" },
+// ! Details of Student with average score above 90
+
+const students = [
+      { name: "Alice", scores: [90, 85, 92] },
+      { name: "Bob", scores: [75, 80, 85] },
+      { name: "Charlie", scores: [90, 95, 85] },
+      { name: "David", scores: [100, 100, 100] }
     ];
 
 
-// ! Department Wise Price.
+//* Target 1:- {Alice:85, Bob:90....}
 
-// * Target One:- {"cat1" : [1,2,3,4], "cat2" : [12,13,14,15]}
-
-const tarOne = products.reduce(function(acc,curr){
-      let currentCategory = curr.category;
-      if (!acc[currentCategory]){
-            acc[currentCategory] = [curr.price];
-      }else{
-            acc[currentCategory].push(curr.price);
-      }
-      return acc;
-},{})
-
-// * Target Two:- {"cat1" : 100; "cat2" : 200...}
-const tarTwo = Object.keys(tarOne).reduce(function (acc,curr){
-      let sumVal = tarOne[curr].reduce(function (sum,price){
-            sum = sum+price;
+const tarOne = students.reduce(function (acc,curr){
+      let currentStudent = curr.name;
+      let currentStudentTotal = curr.scores.reduce(function(sum,runVal){
+            sum = sum + runVal;
             return sum;
       },0)
-      acc[curr] = sumVal;
+      let currentStudentAverage = currentStudentTotal/curr.scores.length;
+
+      // ? Time to store the details of the Student
+      acc[currentStudent] = currentStudentAverage;
       return acc;
 },{})
 
+function finalPush(acc,record){
+      let currentName = record[0];
+      let currentAverage = record[1];
+
+      if (currentAverage > 90){
+            acc.push({'name' : currentName, 'average': currentAverage});
+      }
+      return acc;
+}
+
+const tarTwo = Object.entries(tarOne).reduce(finalPush,[]);
 
 console.log(tarTwo);
-// const output = products.reduce(function (acc,curr){
-//       if (curr.category == 'Electronics'){
-//             acc.push(curr.name);
-//       }
+// const finalOutput = Object.keys(studentsAbove90).reduce(function (acc,curr){
+
 // },[])
-
-// function f(ele){
-//       if (ele.category == "Electronics"){
-//             return true
-//       }else{
-//             return false;
-//       }
-// }
-
-// function f1(ele){
-//       return ele.name;
-// }
-
-// const output = products.filter(f).map(f1);
-
-// console.log(output);
-
-
-// const output = products.reduce(function (acc,curr){
-//       if (curr.category=="Electronics"){
-//             acc.push(curr.name);
-//       }
-//       return acc;
-// },[])
-
-// console.log(output);
 
