90c1ef6f3c68ea035aab85bf449e2b858642c89a
Subhadip Das <subhadipdas911@gmail.com>
2023-12-08 15:12:15 +0530

    Version -01 Integrating Promise with Generators

--- Javascript/Iterator and Generators/integratingPromiseWithGenerators.js
+++ Javascript/Iterator and Generators/integratingPromiseWithGenerators.js
@@ -5,7 +5,7 @@ function download(url){
                   console.log("We are done downloading the data.");
                   let content = "The quick brown fox";
                   resolve(content);
-            },6000)
+            },6000);
       })
 }
 
@@ -13,11 +13,11 @@ function download(url){
 function writeFile(data){
       return new Promise(function exec(resolve,reject){
             console.log("We are now writing the data...",data);
-            setTimeout(function up(){
+            setTimeout(function write(){
                   console.log("We are done writing the data.");
                   let fileName = "file.txt";
                   resolve(fileName);
-            },5000)
+            },5000);
       })
 }
 
@@ -29,9 +29,44 @@ function uploadData(fileName,uploadUrl){
                   console.log("We are done uploading the content");
                   let response = "SUCCESS";
                   resolve(response);
-            },2000)
+            },2000);
       })
 }
 
+// function callback(value){
+//      let future =  iter.next(value);
+//      future.value.then(callback)
+// }
+// 
 
 
+function* steps(){
+      const downloadedData = yield download("www.google.com");
+      console.log("The data downloaded is",downloadedData);
+
+      const writeData = yield writeFile(downloadedData);
+      console.log("The file name is",writeData);
+
+      const uploadResponse = yield uploadData(writeData,"www.dropbox.co.in");
+      console.log("The upload response is",uploadResponse);
+
+      return uploadResponse;
+}
+
+const iter = steps();
+
+const future = iter.next();
+
+future.value
+.then(function callback(data){
+      let result = iter.next(data)
+      return result.value;
+})
+.then(function callback(data){
+      let result = iter.next(data);
+      return result.value;
+})
+.then(function callback(data){
+      let result = iter.next(data);
+      return result.value;
+})
