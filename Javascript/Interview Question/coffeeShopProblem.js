318b039aad8d923bca197aa321b585d9ce007536
Subhadip Das <subhadipdas911@gmail.com>
2023-12-08 20:35:59 +0530

    Approach via Eerror Handling Process

--- Javascript/Interview Question/coffeeShopProblem.js
+++ Javascript/Interview Question/coffeeShopProblem.js
@@ -15,7 +15,7 @@ function placeOrder(order){
                         resolve(response);
                   }else{
                         let response = "NO";
-                        resolve(response);
+                        reject(response);
                   }
             },4000)
       })
@@ -24,52 +24,43 @@ function placeOrder(order){
 
 function processOrder(placeOrderResponse){
       return new Promise(function exec(resolve,reject){
-            if (placeOrderResponse == "NO"){
-                  let response = "Cannot process your Order";
-                  resolve(response);
-            }else{
                   console.log("We are now preparing to process your order...");
                   setTimeout(function callbackTwo(){
                         console.log("We have now processed your order.");
                         let response = "Please take your order";
                         resolve(response);
                   },7000)
-            }
       })
 }
 
 
 function orderBilling(processOrderResponse){
       return new Promise(function exec(resolve,reject){
-            if (processOrderResponse == "Cannot process your Order"){
-                  let response = 0;
-                  resolve(response);
-            }else{
                   console.log("We are printing your Bill...");
                   setTimeout(function callbackThree(){
                         console.log("We are done printing your bill.");
                         let response = "$"+"13.45";
                         resolve(response);
                   },2000)
-            }
       })
 }
 
 
 placeOrder("coffee")
 .then(function one(data){
-      console.log("The Search Result response is",data);
-      return processOrder(data);
+      return processOrder(data)
 })
 .then(function two(data){
       console.log(data);
       return orderBilling(data);
 })
 .then(function three(data){
-      if (data == 0){
-            console.log("You do not have to pay anything");
-      }else{
+
             console.log("Your Bill is",data);
-      }
+      
 })
+.catch(function oneReject(errorMessage){
+      console.log("Order cannot be placed");
+})
+
 
