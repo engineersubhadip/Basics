5d7a4b046e0347ad1656e74c0e023b15b95669a0
Subhadip Das <subhadipdas911@gmail.com>
2023-12-08 22:23:19 +0530

    Updated the code and About to Dry run the output

--- Javascript/Iterator and Generators/Async Await/introductionToAsyncAwait.js
+++ Javascript/Iterator and Generators/Async Await/introductionToAsyncAwait.js
@@ -8,9 +8,9 @@
 
 function getData(){
       return new Promise(function exec(resolve,reject){
-            console.log("We are now fetching your data..."); //*2
+            console.log("We are now fetching your data...");
             setTimeout(function callbackOne(){
-                  console.log("We have fetched your data"); //*3
+                  console.log("We have fetched your data");
                   let response = "IFSC3491";
                   resolve(response);
             },3000)
@@ -19,9 +19,9 @@ function getData(){
 
 function getName(){
       return new Promise(function exec(resolve,reject){
-            console.log("We are now fetching the name of the user..."); //*5
+            console.log("We are now fetching the name of the user...");
             setTimeout(function callbackTwo(){
-                  console.log("We have fetched the name"); //*6
+                  console.log("We have fetched the name");
                   let response = "Hrishikesh";
                   resolve(response);
             },2000)
@@ -29,13 +29,14 @@ function getName(){
 }
 
 async function outer(){
-      console.log("We are inside the Async Function"); //*1
+      console.log("We are inside the Async Function");
       let data1 = await getData();
-      console.log("We have fetched data 1");  //*4
+      console.log("We have fetched data 1");
       let data2 = await getName();
-      console.log("We have fetched data 2"); //*7
+      console.log("We have fetched data 2");
       return [data1,data2];
 }
 
 let response = outer();
-response.then((data)=>{console.log(data)}); //*8
+console.log("This is the response",response);
+response.then((data)=>{console.log(data)});
