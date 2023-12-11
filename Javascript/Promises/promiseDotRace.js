9dfd95b684ed8ec7396b9dc4526b5f81ae415e31
Subhadip Das <subhadipdas911@gmail.com>
2023-12-11 12:51:53 +0530

    Error handling scenario for promiseDotRace.js

---
+++ Javascript/Promises/promiseDotRace.js
@@ -0,0 +1,22 @@
+// Whichever promise gets setteled first that promise Object will be shown
+
+function download(downloadURL,time){
+      return new Promise(function exec(resolve,reject){
+            console.log("We are now downloading the content from the url",downloadURL,"...");
+            setTimeout(function callbackOne(){
+                  console.log("We have downloaded the content from the given url.");
+                  let content = "The Phoenix must burn to emerge";
+                  if (time > 2000){
+                        reject("Cannot Download this");
+                  }else{
+                        resolve(content);
+                  }
+            },time);
+      });
+};
+
+const source1 = download("www.neu.co.us",2500);
+const source2 = download("www.uiuc.co.us",2600);
+const source3 = download("www.asu.co.us",2700);
+
+Promise.race([source1,source2,source3]).then((data) => {console.log("The data is",data)});
