f3b17e863e441133f2bf352d41062774f5e040f8
Subhadip Das <subhadipdas911@gmail.com>
2023-12-11 15:29:16 +0530

    Initial Commit for the file. Included the Problem statement

---
+++ Javascript/Promises/multipleEndPointRequests.js
@@ -0,0 +1,2 @@
+// Imagine you need to fetch data from three different API endpoints: users, posts, and comments. How would you use async/await and Promise.all to make concurrent requests to these endpoints and aggregate the results?
+
