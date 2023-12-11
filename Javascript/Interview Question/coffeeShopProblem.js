ba70800e3a5c4b84c9d3cdd331748a794e9b0e75
Subhadip Das <subhadipdas911@gmail.com>
2023-12-11 19:53:34 +0530

    commit

--- Javascript/Interview Question/coffeeShopProblem.js
+++ Javascript/Interview Question/coffeeShopProblem.js
@@ -7,9 +7,9 @@ Once that is done, he generates a bill and you pay for it.
 
 function placeOrder(order){
       return new Promise(function exec(resolve,reject){
-            console.log("We are checking if your order of",order,"is present or not...");
+            console.log("We are checking if your order of",order,"is present or not..."); //*1
             setTimeout(function callbackOne(){
-                  console.log("Search Complete !");
+                  console.log("Search Complete !"); //*2
                   if (order == "coffee"){
                         let response = "YES !!";
                         resolve(response);
@@ -17,28 +17,28 @@ function placeOrder(order){
                         let response = "NO";
                         reject(response);
                   }
-            },4000)
+            },3000)
       })
 }
 
 
 function processOrder(placeOrderResponse){
       return new Promise(function exec(resolve,reject){
-            console.log("We are now preparing to process your order...");
+            console.log("We are now preparing to process your order..."); //*3
             setTimeout(function callbackTwo(){
-                  console.log("We have now processed your order.");
+                  console.log("We have now processed your order."); //*4
                   let response = "Please take your order";
                   resolve(response);
-            },7000)
+            },3000)
       })
 }
 
 
 function orderBilling(processOrderResponse){
       return new Promise(function exec(resolve,reject){
-            console.log("We are printing your Bill...");
+            console.log("We are printing your Bill..."); //*6
             setTimeout(function callbackThree(){
-                  console.log("We are done printing your bill.");
+                  console.log("We are done printing your bill."); //*7
                   let response = "$"+"13.45";
                   resolve(response);
             },2000)
@@ -54,13 +54,39 @@ placeOrder("tea")
       console.log("Order cannot be placed");
 })
 .then(function two(data){
-      console.log(data);
+      console.log(data); //*5
       return orderBilling(data);
 })
 .then(function three(data){
-      console.log("Your Bill is",data);
+      console.log("Your Bill is",data); //*8
       
 })
+
+
+
+// callstack :- []
+
+// runTime :- []
+
+// callbackque :- []
+
+// microtask :- []
+
+// {
+//       state : reject;
+//       value : no
+// }
+
+// process:- {
+//       state : fulfill;
+//       value:"Please take your order";
+// }
+
+// orderBilling : {
+//       state:fulfill;
+//       value:$13.45
+// }
+
 
 
 
