48cad8a2cc8c7f86baa4ac15758b674ce3c4b76b
Subhadip Das <subhadipdas911@gmail.com>
2024-01-08 21:31:26 +0530

    Implemented Clear Timeout

--- practice/script.js
+++ practice/script.js
@@ -1,6 +1,8 @@
 let target = document.querySelector("div");
 
-setTimeout(function(){
+let timerID = setTimeout(function(){
     target.style.height = "500px";
     target.style.width = "500px";
 },5000);
+
+clearTimeout(timerID);
