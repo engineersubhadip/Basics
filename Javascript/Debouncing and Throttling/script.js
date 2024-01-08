6d8a77631357a11f2a6262df0daa7c70e082ef56
Subhadip Das <subhadipdas911@gmail.com>
2024-01-09 01:13:00 +0530

    Implemented Debouncing

--- Javascript/Debouncing and Throttling/script.js
+++ Javascript/Debouncing and Throttling/script.js
@@ -1,9 +1,27 @@
-let inputBox = document.querySelector("input");
+// The problem with the conventional approach :-
 
+let inputBox = document.querySelector("input");
 let count = 0;
 
-inputBox.addEventListener("keyup",function(e){
+function work(){
     count +=1;
+    console.log("Network Call",count);
+}
 
-    console.log("Network Call made",count);
-})
+// inputBox.addEventListener("keyup",work);
+
+// Method 1:- Debouncing
+
+function debounce(work,delay){
+    let timerID;
+    return function optFn(){
+        clearTimeout(timerID);
+        timerID = setTimeout(function(){
+            work();
+        },delay);
+    };
+};
+
+let optimizedFn = debounce(work,3000);
+
+inputBox.addEventListener("keyup",optimizedFn);
