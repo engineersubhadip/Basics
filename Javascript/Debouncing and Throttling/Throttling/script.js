0e374d373e8ee25b5d1df11bbe2e4d4192819bf6
Subhadip Das <subhadipdas911@gmail.com>
2024-01-09 02:16:53 +0530

    Implemented Throttling

--- Javascript/Debouncing and Throttling/Throttling/script.js
+++ Javascript/Debouncing and Throttling/Throttling/script.js
@@ -1,15 +1,29 @@
 // Conventional Approach :-
-
 let btn = document.querySelector("button");
 let count = 0;
-
 
 function work(){
     count+=1;
     console.log("Count is",count);
 }
 
-btn.addEventListener("click",work);
+// btn.addEventListener("click",work);
 
 // Mitigate via Throttling :-
 
+function throttle(work,delay){
+    let flag = true;
+    return function optimizedFunc(){
+        if (flag){
+            flag = false;
+            timerID = setTimeout(function(){
+                work();
+                flag = true;
+            },delay);
+        }
+    }
+}
+
+let optFunc = throttle(work,2000);
+
+btn.addEventListener("click",optFunc);
