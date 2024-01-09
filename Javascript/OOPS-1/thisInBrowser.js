60012ce8bb14d9a28c7c2640af8cc283a122e08d
Subhadip Das <subhadipdas911@gmail.com>
2024-01-09 15:09:32 +0530

    Ran Direct Invocation in Browser

--- Javascript/OOPS-1/thisInBrowser.js
+++ Javascript/OOPS-1/thisInBrowser.js
@@ -1,1 +1,9 @@
-console.log(this); // Global/ Window Object
+console.log(this); // Global/ Window Object :- All the functionalities given by the Browser lies in this object
+
+// * Direct Invocation
+
+function f(){
+    console.log(this);
+}
+
+f(); // We still get the Global/Window Object
