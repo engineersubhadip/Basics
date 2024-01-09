b6ab89f7d195246bbcebb30a0e41b395c47c0c5e
Subhadip Das <subhadipdas911@gmail.com>
2024-01-09 15:41:21 +0530

    Implemented a Nested scenario where we discussed what the final output will be

--- Javascript/OOPS-1/thisInBrowser.js
+++ Javascript/OOPS-1/thisInBrowser.js
@@ -23,3 +23,18 @@ obj.sayHello(); // This will give us the Parent Object itself
 let result = obj.sayHello;
 
 result(); // This is a DIRECT invocation. So it will be Global/Window Object as the output
+
+let obj1 = {
+    name:"Martha",
+    company:"Stewart Industries",
+    sayHello:function(){
+        function gxn(){
+            console.log(this);
+        }
+        gxn(); // This is a Direct Invocation. It will give us Global/Window Object
+    }
+};
+
+let val2 = obj1.sayHello;
+
+val2(); // This will return us Global/Window Object
