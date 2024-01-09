0982141942ce2f21182ba4dc3597fd8ac1ee39a8
Subhadip Das <subhadipdas911@gmail.com>
2024-01-09 14:56:28 +0530

    Function inside a Function Invocation

--- Javascript/OOPS-1/thisInNodeStrict.js
+++ Javascript/OOPS-1/thisInNodeStrict.js
@@ -25,3 +25,16 @@ obj.sayHello(); // We will get parent object itself
 let result = obj.sayHello;
 
 result(); // This will give us undefined as we are executing the function in STRICT Mode and doing DIRECT Invocation
+
+
+function f(){
+    // console.log(this);
+
+    function g(){
+        console.log(this);
+    }
+
+    g();
+}
+
+f(); // -> This will return undefined
