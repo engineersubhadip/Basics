77df3ff1c1036685ad719b5c9eda9cb88eb07239
Subhadip Das <subhadipdas911@gmail.com>
2024-01-09 13:22:51 +0530

    Direct and Method Invocation

--- Javascript/OOPS-1/thisInNode.js
+++ Javascript/OOPS-1/thisInNode.js
@@ -1,17 +1,17 @@
 // console.log(this); {}
 
 // function f(){
-//     console.log(this); //global Object
+//     console.log(this); //global Object in case of DIRECT Invocation
 // };
 
-// f();
+// f(); // DIRECT Invocation
 
 let obj = {
     name:"Tony",
     age:45,
     sayHello:function(){
-        console.log("Tony says hello");
+        console.log(this); // Object itself in case of METHOD Invocation
     }
 };
 
-obj.sayHello();
+obj.sayHello(); // METHOD Invocation :- Our function is part of an object and we are calling the function via the obj
