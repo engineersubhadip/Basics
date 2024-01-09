ce2ffe876ef33aff451ed5a1bb8350dc1ea0d8ec
Subhadip Das <subhadipdas911@gmail.com>
2024-01-09 13:49:19 +0530

    Calling a function with a DIRECT Invocation via Object

--- Javascript/OOPS-1/thisInNode.js
+++ Javascript/OOPS-1/thisInNode.js
@@ -14,4 +14,8 @@ let obj = {
     }
 };
 
-obj.sayHello(); // METHOD Invocation :- Our function is part of an object and we are calling the function via the obj
+// obj.sayHello(); // METHOD Invocation :- Our function is part of an object and we are calling the function via the obj
+
+let fn = obj.sayHello; // Now fn will have the anonymous function inside of it.
+
+fn(); // When we are invoking the function here, we are making a DIRECT Invocation. So we will get a GLOBAL Object.
