0eb1b269b27a40b2ed2c9dd594c770deba05fea7
Subhadip Das <subhadipdas911@gmail.com>
2024-01-12 11:04:40 +0530

    How we created an object using class

---
+++ Javascript/OOPS-2/class.js
@@ -0,0 +1,12 @@
+// Write a class for a Person having name and age property.
+
+class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    };
+};
+
+const person1 = new Person("Tony",48);
+
+console.log(person1);
