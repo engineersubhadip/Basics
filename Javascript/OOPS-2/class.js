94d90a707f65d001692b4817f1af39f6a7d4d21f
Subhadip Das <subhadipdas911@gmail.com>
2024-01-12 11:38:34 +0530

    Framed the Teacher Class by inheriting from the Student Class

--- Javascript/OOPS-2/class.js
+++ Javascript/OOPS-2/class.js
@@ -1,4 +1,4 @@
-// Write a class for a Person having name and age property.
+// * Write a class for a Person having name and age property.
 
 class Person{
     constructor(name,age){
@@ -11,7 +11,7 @@ const person1 = new Person("Tony",48);
 
 console.log(person1);
 
-// Create a class for Students having name, age and batch property
+// * Create a class for Students having name, age and batch property
 
 class Student extends Person{ // Student class is inheriting from Person class
     constructor(name,age,batch){
@@ -23,3 +23,16 @@ class Student extends Person{ // Student class is inheriting from Person class
 let student1 = new Student("Kat",25,"Academy 22");
 
 console.log(student1);
+
+// * Create a class for Teachers having name, age, batch and class strength property.
+
+class Teacher extends Student{ // Teacher class is inheriting from the Student Class
+    constructor(name,age,batch,strength){
+        super(name,age,batch); // We are invoking the Student Constructor and passing in the arguments expected as a parameter by the student constructor
+        this.strength = strength;
+    };
+};
+
+const teacher1 = new Teacher("Mr. Parker", 48, "Academy DSA", 120);
+
+console.log(teacher1);
