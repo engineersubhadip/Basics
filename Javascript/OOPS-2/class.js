eebf8cae998a5c70cbaecb1ed3d4e69213d88931
Subhadip Das <subhadipdas911@gmail.com>
2024-01-12 11:22:27 +0530

    Student class is inheriting from the Person class

--- Javascript/OOPS-2/class.js
+++ Javascript/OOPS-2/class.js
@@ -13,11 +13,13 @@ console.log(person1);
 
 // Create a class for Students having name, age and batch property
 
-class Student{
+class Student extends Person{ // Student class is inheriting from Person class
     constructor(name,age,batch){
-        this.name = name;
-        this.age = age;
+        super(name,age); // We are invoking the Parent constructor and passing in the arguments expected by the parent constructor
         this.batch = batch;
     }
 };
 
+let student1 = new Student("Kat",25,"Academy 22");
+
+console.log(student1);
