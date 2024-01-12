42caf0d4e389478a31103552fe88f29da05ca955
Subhadip Das <subhadipdas911@gmail.com>
2024-01-12 11:14:32 +0530

    Framing the Student Class without inheritance

--- Javascript/OOPS-2/class.js
+++ Javascript/OOPS-2/class.js
@@ -10,3 +10,14 @@ class Person{
 const person1 = new Person("Tony",48);
 
 console.log(person1);
+
+// Create a class for Students having name, age and batch property
+
+class Student{
+    constructor(name,age,batch){
+        this.name = name;
+        this.age = age;
+        this.batch = batch;
+    }
+};
+
