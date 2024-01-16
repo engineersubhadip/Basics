b3b235d9316cda1cc60592a340d919aa419fbffd
Subhadip Das <subhadipdas911@gmail.com>
2024-01-16 12:56:31 +0530

    Implemented a function that is stored inside a prototype

--- Javascript/OOPS-3/index.js
+++ Javascript/OOPS-3/index.js
@@ -1,6 +1,13 @@
 function Car(name,color){
     this.name = name;
     this.color = color;
+    // this.detail = function(){
+    //     console.log(`Name is ${this.name} and color is ${this.color}`);
+    // }
+}
+
+Car.prototype.detail = function(){
+    console.log(`Name is ${this.name} and color is ${this.color}`);
 }
 
 let bmw = new Car("BMW","gray");
@@ -9,3 +16,5 @@ let nissan = new Car("Nissan","yellow");
 
 console.log(bmw);
 console.log(nissan);
+
+console.log(bmw.detail());
