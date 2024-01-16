bbafdfaef93fbe67d39695f1fc6b233b8cab0f06
Subhadip Das <subhadipdas911@gmail.com>
2024-01-16 12:47:37 +0530

    Object creation via normal Constructor

---
+++ Javascript/OOPS-3/index.js
@@ -0,0 +1,11 @@
+function Car(name,color){
+    this.name = name;
+    this.color = color;
+}
+
+let bmw = new Car("BMW","gray");
+
+let nissan = new Car("Nissan","yellow");
+
+console.log(bmw);
+console.log(nissan);
