190219616a029aa1cfe9b9381f4914ac9298f0e3
Subhadip Das <subhadipdas911@gmail.com>
2024-01-16 13:56:53 +0530

    Initial understanding of CALL

--- Javascript/OOPS-3/index.js
+++ Javascript/OOPS-3/index.js
@@ -19,4 +19,25 @@
 
 // console.log(bmw.detail());
 
-// * Now we will see how to store functions inside an object created without a constructor
+// * Now we will see how to store functions inside an object created without a constructor:-
+
+let obj = {
+    name:"Nissan",
+    color:"Yellow",
+    engine:"V6",
+    detail:function(){
+        console.log("The value of this is",this);
+    }
+};
+
+obj.detail();
+
+let obj1 = {
+    name:"Scaler",
+    color:"blue"
+}
+
+console.log(obj.detail.call(obj1)) 
+
+// `This` keyword inside obj was pointing to obj. Now lets say if we want to use the function detail for obj1 without defining the function inside obj1. What we can do is we can first try to access the function defined inside obj by obj.detail And then do .call(obj1). By doing the last part `this` keyword will now point to obj1
+
