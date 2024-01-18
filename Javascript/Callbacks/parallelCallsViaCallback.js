7980f2f80a73034aaf3bd934fbf0fcca25c985ce
Subhadip Das <subhadipdas911@gmail.com>
2024-01-18 10:58:39 +0530

    Added the Problem statement

---
+++ Javascript/Callbacks/parallelCallsViaCallback.js
@@ -0,0 +1,4 @@
+// Write a function that downloads data from an URl
+// Write a function that saves the downloaded data in a file and return the file name.
+// Write a function that uploads the file written in previous step to a new URL.
+
