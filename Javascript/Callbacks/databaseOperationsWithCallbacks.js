39174cb8e3e2f56660759cb989197fdaad89702b
Subhadip Das <subhadipdas911@gmail.com>
2024-01-19 23:14:27 +0530

    Initial Commit with Problem statement

---
+++ Javascript/Callbacks/databaseOperationsWithCallbacks.js
@@ -0,0 +1,10 @@
+// You are tasked with building a system that interacts with a database. The system needs to perform the following asynchronous operations:
+
+// Query User Data: queryUserData(userId, callback): Queries user data from the database based on the given user ID and calls the callback with the result.
+
+// Query Posts: queryPosts(userId, callback): Queries posts made by a user based on the user ID and calls the callback with the result.
+
+// Query Comments: queryComments(postId, callback): Queries comments for a post based on the post ID and calls the callback with the result.
+
+// Process Data: processData(userData, postsData, commentsData, callback): Processes the queried data and calls the callback with the final result.
+
