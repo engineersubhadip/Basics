69142726c20fa1dc7212c5908c31aa2354732602
Subhadip Das <subhadipdas911@gmail.com>
2024-01-19 22:56:58 +0530

    Initial Commit with Problem statement

---
+++ Javascript/Callbacks/fileOperations.js
@@ -0,0 +1,8 @@
+// You are working on a file processing system. The system needs to perform the following asynchronous tasks:
+
+// Read File: readFile(filePath, callback): Reads the content of a file and calls the callback with the file content.
+
+// Process Data: processData(data, callback): Processes the data read from the file and calls the callback with the processed result.
+
+// Write File: writeFile(filePath, processedData, callback): Writes the processed data to a new file and calls the callback upon completion.
+
