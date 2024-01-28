ad24daad2d1a77a6a135e725c360ada3d127ced7
Subhadip Das <subhadipdas911@gmail.com>
2024-01-28 13:55:51 +0530

    Polyfill of Reduce

---
+++ Javascript/Polyfills/Reduce/script.js
@@ -0,0 +1,22 @@
+let arr = [12,13,14,15];
+
+let result1 = arr.reduce(function(acc,curr){
+    acc = acc + curr;
+    return acc;
+},0);
+
+Array.prototype.customReduce = function(callback,accValue){
+    // customReduce will iterate through each and every element of the array and invoke the callback and store the result inside accumulator
+    let result = accValue;
+    for (let i=0; i<this.length; i++){
+        result = callback(result,this[i]);
+    }
+    return result;
+};
+
+let result2 = arr.customReduce(function(acc,curr){
+    acc = acc + curr;
+    return acc;
+},0);
+
+console.log(result2);
