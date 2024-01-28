a848df0d706b6351ed984d4a859786135d59371b
Subhadip Das <subhadipdas911@gmail.com>
2024-01-28 19:52:54 +0530

    Polyfill of indexOf

---
+++ Javascript/Polyfills/indexOf/script.js
@@ -0,0 +1,16 @@
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison','duck'];
+
+console.log(beasts.indexOf('bison'));
+
+Array.prototype.customIndexOf = function(targetElement){
+    let resultIndex = -1;
+    for (let i=0; i<this.length; i++){
+        if (this[i] == targetElement){
+            resultIndex = i;
+            break;
+        }
+    }
+    return resultIndex;
+};
+
+console.log(beasts.customIndexOf("duck"));
