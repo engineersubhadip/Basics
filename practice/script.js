9a72eaa70be453e65e07ce2ad6826ca13ce3d6f9
Subhadip Das <subhadipdas911@gmail.com>
2024-01-31 20:07:42 +0530

    Basic Constructor Function

--- practice/script.js
+++ practice/script.js
@@ -1,11 +1,18 @@
-let obj = {
-    a : function(){
-        function b(){
-            console.log(this);
-        }
-        b();
-    }
+function Car(name,price,top_speed){
+    console.log(this);
+    this.name = name;
+    this.price = price;
+    this.top_speed = top_speed;
+    // console.log(this) -> {name:"Nissan",price:1230000, top_speed:"260Km/hr"}
+    console.log(this);
 }
 
-let x = obj.a;
-x();
+
+let nissan = new Car("Nissan",1230000,"260Km/hr");
+
+let toyota = new Car("Toyota",1000000,"188Km/hr");
+
+console.log(nissan);
+
+console.log(toyota);
+
