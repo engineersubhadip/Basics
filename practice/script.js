61cde04829bba2160fa1647b63fc3c44ea535206
Subhadip Das <subhadipdas911@gmail.com>
2024-02-01 20:26:59 +0530

    Created Another class without using inheriting properties from another class

--- practice/script.js
+++ practice/script.js
@@ -12,3 +12,16 @@ console.log(person1);
 
 
 
+// We create a Student class with name,age,batch property
+
+class Student{
+    constructor(name,age,batch){
+        this.name = name;
+        this.age = age;
+        this.batch = batch;
+    }
+}
+
+let student1 = new Student("Parikh",25,"Academy DSML");
+
+console.log(student1);
