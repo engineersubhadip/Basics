1647bea2fd2d5572b47380bcf8842087399d2b83
Subhadip Das <subhadipdas911@gmail.com>
2024-02-01 20:14:25 +0530

    Basic Class Format

--- practice/script.js
+++ practice/script.js
@@ -1,25 +1,14 @@
-// Write a constructor function for a Person having name and age property.
-
-function Person(name,age){
-    // this keyword will point to an empty object
-    this.name = name;
-    this.age = age;
-    // The default return type is "this" keyword
-    // {name:"Subhadip", age:28} -> Person1
-    // {name:"Aman", age:30} -> Person2
+class Person{
+    constructor(name,age){
+        console.log(this);
+        this.name = name;
+        this.age = age;
+    }
 }
 
-let person1 = new Person("Subhadip",28);
-
-let person2 = new Person("Aman",30);
+let person1 = new Person("Subhadip",29);
 
 console.log(person1);
 
-console.log(person2);
 
-Person.prototype.sayHello = function(){
-    console.log(this.name+" says hello there !!");
-}
 
-person1.sayHello();
-person2.sayHello();
