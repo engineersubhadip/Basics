5789cadb318194e28290ca28124d24f39b1df50a
Subhadip Das <subhadipdas911@gmail.com>
2024-02-01 23:08:26 +0530

    Added function definition to the prototype of constructor function. So that objects of the constructor function can inherit from there

--- practice/script.js
+++ practice/script.js
@@ -3,9 +3,13 @@
 function Car(name,speed){
     this.name = name;
     this.speed = speed;
-    this.detail = function(){
-        console.log(`The speed is ${this.speed} and the name is ${this.name}`);
-    }
+    // this.detail = function(){
+    //     console.log(`The speed is ${this.speed} and the name is ${this.name}`);
+    // }
+}
+
+Car.prototype.detail = function(){
+    console.log(`The speed is ${this.speed} and the name is ${this.name}`);
 }
 
 let c1 = new Car("Bmw",320);
@@ -15,3 +19,7 @@ let c2 = new Car("Nissan",180);
 console.log(c1);
 
 console.log(c2);
+
+c1.detail();
+
+c2.detail();
