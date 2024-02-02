c6e11190a7cb7a8c935d4808184702256442d84d
Subhadip Das <subhadipdas911@gmail.com>
2024-02-02 16:33:11 +0530

    Implemented Scneario 5 via the usage of Call Method

--- Javascript/OOPS-3/scenarioBasedCall5.js
+++ Javascript/OOPS-3/scenarioBasedCall5.js
@@ -1,2 +1,20 @@
 // Prototype Chain Usage:
 // Consider a scenario where you have a prototype object shape with a method area. You also have another object rectangle that has properties width and height, and it should inherit the area method from shape. How would you utilize the call method to link rectangle with shape?
+
+let shape = {
+    area:function(width,height){
+        if (this.width != undefined && this.height != undefined){
+            return this.width * this.height;
+        }else{
+            return "This function accepts width and height"
+        }
+    }
+}
+
+let rectangle = {
+    width:25,
+    height:12
+}
+
+let result = shape.area.call(rectangle);
+console.log(result);
