21cdc39a9d44d701c10ccce9abc828d8e2a62d19
Subhadip Das <subhadipdas911@gmail.com>
2024-02-02 13:53:41 +0530

    Object2 is borrowing function from Object1 via Call Method

--- Javascript/OOPS-3/index.js
+++ Javascript/OOPS-3/index.js
@@ -1,17 +1,35 @@
-function Car(name,color){
-    this.name = name;
-    this.color = color;
+// function Car(name,color){
+//     this.name = name;
+//     this.color = color;
+// }
+
+// Car.prototype.detail = function(){
+//     console.log(`Name of the car is ${this.name} and the color is ${this.color}`);
+// }
+
+// let c1 = new Car("BMW","Red");
+// let c2 = new Car("Ferarri","Black");
+
+// console.log(c1);
+// console.log(c2);
+
+// c1.detail();
+// c2.detail();
+
+
+let person1 = {
+    name:"Tharun",
+    age:25,
+    greet:function(){
+        console.log("Value of this keyword is",this);
+    }
 }
 
-Car.prototype.detail = function(){
-    console.log(`Name of the car is ${this.name} and the color is ${this.color}`);
+person1.greet();
+
+let person2 = {
+    name:"Aman",
+    age:26
 }
 
-let c1 = new Car("BMW","Red");
-let c2 = new Car("Ferarri","Black");
-
-console.log(c1);
-console.log(c2);
-
-c1.detail();
-c2.detail();
+person1.greet.call(person2);
