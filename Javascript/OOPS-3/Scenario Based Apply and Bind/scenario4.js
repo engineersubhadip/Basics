8effc7b7ff451777b0d48fe6f4e79bafa3fec2cd
Subhadip Das <subhadipdas911@gmail.com>
2024-02-02 22:31:17 +0530

    Initial Commit

---
+++ Javascript/OOPS-3/Scenario Based Apply and Bind/scenario4.js
@@ -0,0 +1,3 @@
+// Scenario 6: Delayed Execution
+
+// You have a function printMessage that logs a message to the console. Create a new function delayedPrint using bind that delays the execution of printMessage by 2 seconds.
