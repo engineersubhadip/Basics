98d747487447a00c458dfa90bb13bd242dde0f64
Subhadip Das <subhadipdas911@gmail.com>
2024-02-02 20:44:47 +0530

    Staged the unchanged Deleted Files to reflect

--- practice/script.js
+++ practice/script.js
@@ -1,21 +1,27 @@
-let arr1 = [12,13,14,14]
-
-// let result = arr1.map(function(ele){
-//     return ele*ele;
-// });
-// console.log(result);
-
-Array.prototype.customMap = function(callback){
-    let result = [];
-    for (let i=0; i<this.length; i++){
-        let currRes = callback(this[i]);
-        result.push(currRes);
-    }
-    return result;
+let car = {
+    name: "Mercedes",
+    color: "White",
+};
+  
+function buyCar() {
+    console.log(`I bought a ${this.color} ${this.name}`);
 }
 
-let val1 = arr1.customMap(function(ele){
-    return ele*ele;
-});
+buyCar.call(car, 100000);
 
-console.log(val1);
+Function.prototype.myCall = function(obj, ...args){
+    console.log(this); // it's a buyCar function. 
+    console.log(obj); // { name: 'Mercedes', color: 'White' }
+    obj.fxn = this;
+    //obj = {
+    //     name:"Mercedes",
+    //     color:"White",
+    //     fxn:function buyCar(){
+    //         console.log(`I bought a ${this.color} ${this.name}`);
+    //     }
+    // }
+    // console.log(obj);
+    obj.fxn(...args);
+}
+
+buyCar.myCall(car);
