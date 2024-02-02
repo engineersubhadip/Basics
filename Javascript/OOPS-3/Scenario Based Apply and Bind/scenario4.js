ddabaf9a7f0c7cba51064404b04a5101ea522ea2
Subhadip Das <subhadipdas911@gmail.com>
2024-02-02 22:34:41 +0530

    Implemented Bind along with the Usage of SetTimeout

--- Javascript/OOPS-3/Scenario Based Apply and Bind/scenario4.js
+++ Javascript/OOPS-3/Scenario Based Apply and Bind/scenario4.js
@@ -1,3 +1,11 @@
 // Scenario 6: Delayed Execution
 
 // You have a function printMessage that logs a message to the console. Create a new function delayedPrint using bind that delays the execution of printMessage by 2 seconds.
+
+function printMessage(){
+    console.log("Hello there, how are you ?");
+}
+
+let delayedPrint = printMessage.bind(null);
+
+setTimeout(delayedPrint,2000);
