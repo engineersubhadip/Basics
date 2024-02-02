8063f4f1aa2966643ddd49dd2b9c626f142acd3d
Subhadip Das <subhadipdas911@gmail.com>
2024-02-02 14:13:41 +0530

    Setting new keys for Object2 via the usage of Call method

--- Javascript/OOPS-3/index.js
+++ Javascript/OOPS-3/index.js
@@ -20,16 +20,24 @@
 let person1 = {
     name:"Tharun",
     age:25,
-    greet:function(){
+    greet:function(batch1,batch2){
         console.log("Value of this keyword is",this);
+        this.batch1 = batch1;
+        this.batch2 = batch2;
     }
 }
 
-person1.greet();
+person1.greet("Web","DSA");
 
 let person2 = {
     name:"Aman",
     age:26
 }
 
-person1.greet.call(person2);
+// person1.greet.call(person2); // We are changing the context of this keyword to person2
+
+// person2 also wants to borrow greet()
+
+person1.greet.call(person2,"Backend","Kubernetes");
+
+console.log(person2);
