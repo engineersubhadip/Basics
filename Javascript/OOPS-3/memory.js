81588f88120a520930b04ec161c6f2283da375f9
Subhadip Das <subhadipdas911@gmail.com>
2024-02-04 13:17:18 +0530

    Explanation of how 3 objects are pointing and updating their respective Heap Memory Reference Address

--- Javascript/OOPS-3/memory.js
+++ Javascript/OOPS-3/memory.js
@@ -11,17 +11,17 @@
 
 // * Reference
 
-let obj1 = {
-    name:"Alex"
-}
+// let obj1 = {
+//     name:"Alex"
+// }
 
-let obj2 = obj1;
+// let obj2 = obj1;
 
-obj2.name = "Adam";
+// obj2.name = "Adam";
 
-console.log(obj1);
+// console.log(obj1);
 
-console.log(obj2);
+// console.log(obj2);
 
 // variable Obj1 gets created inside the callstack. What value we are storing inside Obj1 will depend whether the value is of Primitive/Reference Data type. We are trying to store a reference data type. If we are storing a reference data type inside Obj1. The reference data type gets stored inside the Heap Memory and obj1 stores the heap memory reference of obj1. Meaning Obj1 is pointing to the {name:"Alex"}(present inside the Heap) via the memory reference address.
 // We do obj2 = obj1. Variable Obj2 gets created inside the Callstack and what we are storing inside Obj2 is Obj1. What is the value of Obj1 ? -> It is the heap memory reference address. So the value of Obj2 is the same heap memory reference stored inside Obj1.
@@ -30,3 +30,30 @@ console.log(obj2);
 
 // **Note**
 // If the value to be stored is a Primitive data type, then it would have been stored inside the Callstack itself
+
+let obj1 = {name:"Alex"};
+
+let obj3 = {name:"Starc"};
+
+let obj2 = obj1;
+
+obj2.name = "Adam";
+
+obj2 = obj3;
+
+obj3.name = "Hales";
+
+console.log(obj1);
+
+console.log(obj2);
+
+console.log(obj3);
+
+// Explanation:-
+// Obj1 ->{name:Alex}
+// Obj3 ->{name:Starc}
+// Obj2 -> {name:Alex}
+// Obj2.name = Adam -> Obj2 : {name:Adam} ; Obj1 : {name:Adam}
+// Obj2 = Obj3 -> We sever the existing Obj2 connection and now Obj2 -> {name:Starc}
+// Obj3.name = Hales -> Obj3 : {name:Hales} and Obj2 : {name:Hales};
+// Therefore, Obj1 -> {name:Adam} ; Obj2 -> {name: Hales}; Obj3 -> {name: Hales}
