99c32a099280943454b6e68fe15d40c27ed440fd
Subhadip Das <subhadipdas911@gmail.com>
2024-02-05 12:13:34 +0530

    PolyFill of Filter Method

--- practice/script.js
+++ practice/script.js
@@ -18,25 +18,60 @@
 
 // * Polyfill of Map:-
 
-let array = [12,13,14,15,188];
+// let array = [12,13,14,15,188];
 
-// let result = array.map(function(ele){
-//     return ele*ele;
-// })
+// // let result = array.map(function(ele){
+// //     return ele*ele;
+// // })
+
+// // console.log(result);
+
+// Array.prototype.customMap = function(callback){
+//     let result = [];
+//     for (let i=0; i<this.length; i++){
+//         let currVal = callback(this[i]);
+//         result.push(currVal);
+//     }
+//     return result;
+// }
+
+// let finalOutput = array.customMap(function(ele){
+//     return ele*ele*ele;
+// });
+
+// console.log(finalOutput);
+
+// * PolyFill of Filter:-
+
+let array = [20,23,27,88,121];
+
+// let result = array.filter(function(ele){
+//     if (ele % 2 == 0){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// });
 
 // console.log(result);
 
-Array.prototype.customMap = function(callback){
+Array.prototype.customFilter = function(callback){
     let result = [];
     for (let i=0; i<this.length; i++){
-        let currVal = callback(this[i]);
-        result.push(currVal);
+        let currResult = callback(this[i]);
+        if (currResult){
+            result.push(this[i]);
+        }
     }
     return result;
-}
+};
 
-let finalOutput = array.customMap(function(ele){
-    return ele*ele*ele;
+let output = array.customFilter(function(ele){
+    if (ele % 2 == 0){
+        return true;
+    }else{
+        return false;
+    }
 });
 
-console.log(finalOutput);
+console.log(output);
