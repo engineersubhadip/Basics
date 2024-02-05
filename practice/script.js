075a1847a71c35f2967698318bb83dfad957516b
Subhadip Das <subhadipdas911@gmail.com>
2024-02-05 12:04:45 +0530

    Polyfill for For Each

--- practice/script.js
+++ practice/script.js
@@ -1,36 +1,17 @@
-function download(userUrl,callback){
-    console.log("We are now downloading from the URL",userUrl);
-    setTimeout(function f1(){
-        console.log("We have downloaded the content");
-        let content = "Ishq Nachawe";
-        callback(content);
-    },3000);
-}
+// * PolyFill of ForEach
 
+let array = [12,13,14,15]
 
-function saveContent(content,callback){
-    console.log("We are now saving the content...");
-    setTimeout(function f2(){
-        callback();
-    },2000);
-}
+// array.forEach(function(ele){
+//     console.log(ele*ele);
+// });
 
-function uploadContent(content,uploadURL,callback){
-    console.log("We are now uploading the content to ->",uploadURL);
-    setTimeout(function f3(){
-        console.log("We have uploaded the content");
-        let message = "Success";
-        callback(message);
-    },2000);
-}
+Array.prototype.customForEach = function(callback){
+    for (let i=0; i<this.length; i++){
+        callback(this[i]);
+    }
+};
 
-download("www.gyfl.co.in",function cb1(content){
-    console.log("The downloaded content is ->",content);
-    saveContent(content,function cb3(){
-        console.log("We have saved the content");
-        uploadContent(content,"www.dropbox.co",function cb2(message){
-            console.log("We have uploaded the content");
-            console.log("The message is ->",message);
-        });
-    });
-});
+array.customForEach(function(ele){
+    console.log(ele**3);
+})
