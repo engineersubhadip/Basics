9f0be73961dc7c9f39f12d724bb4265d40067a55
Subhadip Das <subhadipdas911@gmail.com>
2024-02-05 11:14:11 +0530

    Mimicking Callback Hell by performing Download followed by Saving the content and then Uplaoding the content

--- practice/script.js
+++ practice/script.js
@@ -1,18 +1,36 @@
-function calculate(a,b){
-    return a+b;
+function download(userUrl,callback){
+    console.log("We are now downloading from the URL",userUrl);
+    setTimeout(function f1(){
+        console.log("We have downloaded the content");
+        let content = "Ishq Nachawe";
+        callback(content);
+    },3000);
 }
 
-let obj1 = {
-    val1:10,
-    val2:20
+
+function saveContent(content,callback){
+    console.log("We are now saving the content...");
+    setTimeout(function f2(){
+        callback();
+    },2000);
 }
 
-let obj2 = {
-    val1:22,
-    val2:33
+function uploadContent(content,uploadURL,callback){
+    console.log("We are now uploading the content to ->",uploadURL);
+    setTimeout(function f3(){
+        console.log("We have uploaded the content");
+        let message = "Success";
+        callback(message);
+    },2000);
 }
 
-console.log(calculate.call(obj1,obj1.val1,obj1.val2));
-
-console.log(calculate.call(obj2,obj2.val1,obj2.val2));
-
+download("www.gyfl.co.in",function cb1(content){
+    console.log("The downloaded content is ->",content);
+    saveContent(content,function cb3(){
+        console.log("We have saved the content");
+        uploadContent(content,"www.dropbox.co",function cb2(message){
+            console.log("We have uploaded the content");
+            console.log("The message is ->",message);
+        });
+    });
+});
