aecd345e96852f7a8c04f1c8fb8c1bab44d4b55b
Subhadip Das <subhadipdas911@gmail.com>
2024-02-05 12:09:32 +0530

    PolyFill for Map Method

--- practice/script.js
+++ practice/script.js
@@ -1,17 +1,42 @@
 // * PolyFill of ForEach
 
-let array = [12,13,14,15]
+// let array = [12,13,14,15]
 
-// array.forEach(function(ele){
-//     console.log(ele*ele);
-// });
+// // array.forEach(function(ele){
+// //     console.log(ele*ele);
+// // });
 
-Array.prototype.customForEach = function(callback){
+// Array.prototype.customForEach = function(callback){
+//     for (let i=0; i<this.length; i++){
+//         callback(this[i]);
+//     }
+// };
+
+// array.customForEach(function(ele){
+//     console.log(ele**3);
+// })
+
+// * Polyfill of Map:-
+
+let array = [12,13,14,15,188];
+
+// let result = array.map(function(ele){
+//     return ele*ele;
+// })
+
+// console.log(result);
+
+Array.prototype.customMap = function(callback){
+    let result = [];
     for (let i=0; i<this.length; i++){
-        callback(this[i]);
+        let currVal = callback(this[i]);
+        result.push(currVal);
     }
-};
+    return result;
+}
 
-array.customForEach(function(ele){
-    console.log(ele**3);
-})
+let finalOutput = array.customMap(function(ele){
+    return ele*ele*ele;
+});
+
+console.log(finalOutput);
