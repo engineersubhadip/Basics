0da0293b3df67aa448d49c3c5d2c3caa705dcf63
Subhadip Das <subhadipdas911@gmail.com>
2024-02-06 13:41:48 +0530

    Fixed a bug -> Implemented Strict Equality Operator instead of Abstract Equality Operator

--- Javascript/Polyfills/indexOf/script.js
+++ Javascript/Polyfills/indexOf/script.js
@@ -5,7 +5,7 @@ console.log(beasts.indexOf('bison'));
 Array.prototype.customIndexOf = function(targetElement){
     let resultIndex = -1;
     for (let i=0; i<this.length; i++){
-        if (this[i] == targetElement){
+        if (this[i] === targetElement){
             resultIndex = i;
             break;
         }
