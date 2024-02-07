aa715242b62fd1fb04e9a3302cf71f59c0d19e92
Subhadip Das <subhadipdas911@gmail.com>
2024-02-07 13:14:07 +0530

    Implemented Call to change the context of this from one function to another

--- practice/script.js
+++ practice/script.js
@@ -1,27 +1,24 @@
-// You have an object representing a person with properties like name and age. You also have a method called introduce within the object that prints a greeting message along with the person's name and age. Explain how you would use call, apply, and bind to introduce another person using the same introduce method.
-
-let obj1 = {
-    name:"Tharun",
-    age:29,
-    introduce:function(){
-        console.log(`Good Morning ${this.name} of age ${this.age}`);
-    }
+function a(){
+    console.log("The value of this is "+this+" inside function a");
 }
 
-let obj2 = {
-    name:"Guru",
-    age:28
+function b(){
+    console.log("The value of this is "+this+" inside function b");
 }
 
-let obj3 = {
-    name:"Nikita",
-    age:27
-}
 
-obj1.introduce.call(obj2);
+b.call(a);
 
-obj1.introduce.call(obj3);
 
-// Here we are setting the context of "this" keyword inside the function, to the argument we are passing inside the call method().
-// For the first output the context of "this" keyword is pointing to obj2. And hence it is taking out name and age from there.
-// For the second output the context of "this" keyword is pointing to obj3. And hence it is taking out name and age from there.
+// The context of "this" inside function b is pointing to function a(){
+//     console.log("The value of this is "+this+" inside function a");
+// }
+
+// Meaning this ->  function a(){
+//     console.log("The value of this is "+this+" inside function a");
+// }
+
+// Therefore the output will be
+// The value of this is function a(){
+//     console.log("The value of this is "+this+" inside function a");
+// } inside function b
