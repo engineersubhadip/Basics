cbef0d020cfc7abeeeed1295c6af5001f6dc3ee1
Subhadip Das <subhadipdas911@gmail.com>
2024-02-07 13:00:37 +0530

    Implemented Call Method to set the context of this to other objects

--- practice/script.js
+++ practice/script.js
@@ -1,34 +1,27 @@
-// * Reduce PolyFill:-
+// You have an object representing a person with properties like name and age. You also have a method called introduce within the object that prints a greeting message along with the person's name and age. Explain how you would use call, apply, and bind to introduce another person using the same introduce method.
 
-let array = [12,13,14,91,22,12,13,13];
-
-// let result = array.reduce(function(acc,curr){
-//     if (acc[curr] == undefined){
-//         acc[curr]=1;
-//     }else{
-//         acc[curr]+=1;
-//     }
-//     return acc;
-// },{});
-
-// console.log(result);
-
-Array.prototype.customReduce = function(callback,initialVal){
-    let result = initialVal;
-
-    for (let i=0; i<this.length; i++){
-        result = callback(result,this[i]);
+let obj1 = {
+    name:"Tharun",
+    age:29,
+    introduce:function(){
+        console.log(`Good Morning ${this.name} of age ${this.age}`);
     }
-    return result;
 }
 
-let output = array.customReduce(function(acc,curr){
-    if (acc[curr] == undefined){
-        acc[curr]=1;
-    }else{
-        acc[curr]+=1;
-    }
-    return acc;
-},{});
+let obj2 = {
+    name:"Guru",
+    age:28
+}
 
-console.log(output);
+let obj3 = {
+    name:"Nikita",
+    age:27
+}
+
+obj1.introduce.call(obj2);
+
+obj1.introduce.call(obj3);
+
+// Here we are setting the context of "this" keyword inside the function, to the argument we are passing inside the call method().
+// For the first output the context of "this" keyword is pointing to obj2. And hence it is taking out name and age from there.
+// For the second output the context of "this" keyword is pointing to obj3. And hence it is taking out name and age from there.
