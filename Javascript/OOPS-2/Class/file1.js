7559262028356584de81455ca2264759cfd67897
Subhadip Das <subhadipdas911@gmail.com>
2024-02-09 21:22:22 +0530

    How priority is being set for the same function present as a key and inside the prototype

--- Javascript/OOPS-2/Class/file1.js
+++ Javascript/OOPS-2/Class/file1.js
@@ -39,3 +39,26 @@ console.log(bmwCar2.model); // BMW
 console.log(bmwCar2.color); // Red
 
 console.log(bmwCar2.getDetail()); // JS checked in the first level that the function is not present. So it went inside Prototype and found the function.
+
+console.log(Car2);
+
+
+class Car3{
+    constructor(model,color){
+        this.model = model;
+        this.color = color;
+        this.getDetail = function(){ // It will be created as a key inside the top most object
+            return `Model will be ${this.model} and color will be ${this.color}`;
+        }
+    }
+
+    getDetail(){ // This will be saved inside the Prototype
+        return `Model is ${this.model} and color is ${this.color}`;
+    }
+};
+
+let bmwCar3 = new Car3("BMW","Red");
+
+console.log(bmwCar3);
+
+console.log(bmwCar3.getDetail()); // It will execute line 51. Because, that function is present as a key inside the top most object. Whereas, the method getDetail in line 55 will be present inside the prototype of the object bmwCar3
