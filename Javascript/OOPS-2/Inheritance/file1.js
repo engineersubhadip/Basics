3f78d45cff2dca13a5e1a74aa5e42b0c1d1b7d5f
Subhadip Das <subhadipdas911@gmail.com>
2024-02-09 22:45:56 +0530

    During Inheritance how the prototype Chaining is being formed

--- Javascript/OOPS-2/Inheritance/file1.js
+++ Javascript/OOPS-2/Inheritance/file1.js
@@ -19,3 +19,12 @@ class SuperCar extends Car{ // SuperCar will inherit from Car
 let bmwSuperCar = new SuperCar("BMW","Red",["f1","f2"],2300000);
 
 console.log(bmwSuperCar);
+
+
+// bmwSuperCar -> {model:"BMW",color:"Red",getDetails:f,features:["f1","f2"],price:230000}
+// bmwSuperCar object is built from the Blueprint of SuperCar Class.
+// So inside the prototype we will have the constructor function of SuperClass.
+// Parent on SuperClass will be Car . So inside the Prototype of SuperClass we will have constructor function of Car.
+// Car Class will be built on top of Native JS Object. So inside the prototype of Car we will have Native JS Object's functionalities
+
+// {model:"BMW",color:"Red",getDetails:f,features:["f1","f2"],price:230000, Prototype : constructor of SuperClass -> Prototype : constructor of Car -> Prototype of Object}
