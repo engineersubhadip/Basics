daacce9c00341a95c41bc29a5731ddabb7d38d62
Subhadip Das <subhadipdas911@gmail.com>
2024-02-09 13:30:05 +0530

    We are setting the context of this keyword to multiple objects at the same time

--- Javascript/OOPS-1/ExampleSet/example1.js
+++ Javascript/OOPS-1/ExampleSet/example1.js
@@ -20,3 +20,7 @@ let x = obj.myTest;
 x(); // We will get global object, because we are making function invocation
 
 x.call(obj) // The context of this in the function will now refer to obj being passed as an argument inside call
+
+let obj1 = {name:"Pushkar"}
+
+x.call(obj1) // Here we are setting the context of this to obj1 being passed as an argument inside the call
