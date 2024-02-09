4b5a34d2a8b633cf63925d3b3850b17e222d7a1b
Subhadip Das <subhadipdas911@gmail.com>
2024-02-09 20:38:56 +0530

    How we are storing methods inside the Prototype and while rendering how JS is finding out the value of key

--- Javascript/OOPS-2/Class/file1.js
+++ Javascript/OOPS-2/Class/file1.js
@@ -1,4 +1,4 @@
-class Car{
+class Car1{
     constructor(model,color){
         this.model = model;
         this.color = color;
@@ -8,7 +8,7 @@ class Car{
     }
 };
 
-let bmwCar = new Car("BMW","Red");
+let bmwCar = new Car1("BMW","Red");
 
 console.log(bmwCar); // bmwCar is an object created from the Class Car.
 
@@ -17,3 +17,25 @@ console.log(bmwCar.color);
 console.log(bmwCar.model);
 
 console.log(bmwCar.getDetail()); // We are doing a method invocation at this point. So the value of "this" inside the function will point to the object through which the method is getting called
+
+
+class Car2{
+    constructor(model,color){
+        this.model = model;
+        this.color = color;
+    }
+
+    getDetail(){ // This will be saved inside the Prototype
+        return `Model is ${this.model} and color is ${this.color}`;
+    }
+};
+
+let bmwCar2 = new Car2("BMW","Red");
+
+console.log(bmwCar2); // -> {model : BMW, color : Red}
+
+console.log(bmwCar2.model); // BMW
+
+console.log(bmwCar2.color); // Red
+
+console.log(bmwCar2.getDetail()); // JS checked in the first level that the function is not present. So it went inside Prototype and found the function.
