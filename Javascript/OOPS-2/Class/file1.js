e95481327386a1c28f8e16759ac63a766ceb16b3
Subhadip Das <subhadipdas911@gmail.com>
2024-02-09 20:17:48 +0530

    Explained how we are able to access the properties of object via Method Invocation

--- Javascript/OOPS-2/Class/file1.js
+++ Javascript/OOPS-2/Class/file1.js
@@ -10,4 +10,10 @@ class Car{
 
 let bmwCar = new Car("BMW","Red");
 
-console.log(bmwCar);
+console.log(bmwCar); // bmwCar is an object created from the Class Car.
+
+console.log(bmwCar.color);
+
+console.log(bmwCar.model);
+
+console.log(bmwCar.getDetail()); // We are doing a method invocation at this point. So the value of "this" inside the function will point to the object through which the method is getting called
