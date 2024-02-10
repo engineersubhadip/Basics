746cc4745fa26581a26f02092314f87819f0f1c1
Subhadip Das <subhadipdas911@gmail.com>
2024-02-10 23:25:40 +0530

    Implemented Custom Filer PolyFill

--- practice/script.js
+++ practice/script.js
@@ -1,16 +1,28 @@
-function sleep(delay){
-    delay = delay *1000;
-    return new Promise(function(resolve,reject){
-    setTimeout(function f(){
-    resolve();
-    },delay)
-    })
-}
-async function x(){
-    console.log(1)
-    await sleep(5)
-    console.log(8);
+let array = [12,13,14,15,15,16];
+
+// let result = array.filter(function(ele){
+//     if (ele % 2 == 0){
+//         return true
+//     }
+// })
+
+// console.log(result);
+
+Array.prototype.customFilter = function(callback){
+    let result = [];
+    for (let i=0; i<this.length; i++){
+        let currVal = callback(this[i]);
+        if (currVal){
+            result.push(this[i]);
+        }
+    }
+    return result;
 }
 
+let val = array.customFilter(function(ele){
+    if (ele % 2 == 0){
+        return true
+    }
+});
 
-x();
+console.log(val);
