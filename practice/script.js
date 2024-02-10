d05318ed6df00daebc0a7a6a08aa8c3309a27471
Subhadip Das <subhadipdas911@gmail.com>
2024-02-10 22:56:20 +0530

    Implemented Sleep Function via async await

--- practice/script.js
+++ practice/script.js
@@ -1,5 +1,16 @@
-let [a,b,c] = [1,2,3];
+function sleep(delay){
+    delay = delay *1000;
+    return new Promise(function(resolve,reject){
+    setTimeout(function f(){
+    resolve();
+    },delay)
+    })
+}
+async function x(){
+    console.log(1)
+    await sleep(5)
+    console.log(8);
+}
 
-console.log(a);
-console.log(b);
-console.log(c);
+
+x();
