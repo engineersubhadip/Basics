cf0d477d23e00c3cf26d9501449cf5b705167807
Subhadip Das <subhadipdas911@gmail.com>
2024-02-10 21:59:33 +0530

    Array destructuring

--- practice/script.js
+++ practice/script.js
@@ -1,24 +1,5 @@
-function a(){
-    console.log("The value of this is "+this+" inside function a");
-}
+let [a,b,c] = [1,2,3];
 
-function b(){
-    console.log("The value of this is "+this+" inside function b");
-}
-
-
-b.call(a);
-
-
-// The context of "this" inside function b is pointing to function a(){
-//     console.log("The value of this is "+this+" inside function a");
-// }
-
-// Meaning this ->  function a(){
-//     console.log("The value of this is "+this+" inside function a");
-// }
-
-// Therefore the output will be
-// The value of this is function a(){
-//     console.log("The value of this is "+this+" inside function a");
-// } inside function b
+console.log(a);
+console.log(b);
+console.log(c);
