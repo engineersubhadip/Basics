b857445f0913d799e3145e98e412e08765020575
Subhadip Das <subhadipdas911@gmail.com>
2024-02-12 19:36:36 +0530

    Removed un-necessary code and also rectified a missing key inside the object of obj1 and obj2

--- Javascript/OOPS-2/constructorFnc.js
+++ Javascript/OOPS-2/constructorFnc.js
@@ -1,17 +1,3 @@
-// let marriage = {
-//     name:"Atul",
-//     age:29,
-//     company:true,
-//     address:{
-//         city:"Patna"
-//     },
-//     getDetails:function(){
-//         console.log(`Name is ${this.name} and currently working is ${this.company}`);
-//     }
-// }
-
-// marriage.getDetails();
-
 // * Creating Object via Constructor :-
 
 function Person(name,age){
@@ -32,47 +18,12 @@ console.log(obj2);
 
 
 // How obj1 and obj2 will look like :-
-// obj1 -> {name :"Atul", age:29}
+// obj1 -> {name :"Atul", age:29, getDetail:f}
 // Inside the Prototype of Obj1 we will have constructor : f(Person) and Prototype.
 // Inside the 2nd Prototype we will have Native JS Object Methods.
 // Same for Obj2
 
 
-// class Car{
-    
-//     constructor(model,color){
-//         this.model = model;
-//         this.color = color;
-//         this.getDetail = function(){ // This will get added as a key to the object
-//             return `The model is ${this.model} and the color is ${this.color}`;
-//         }
-//     }
-
-//     getModel(){ // This will get added to the Prototype
-//         return `The model is ${this.model}`;
-//     }
-// }
-
-// class SuperCar extends Car{ // SuperCar is inheriting from Car
-
-//     constructor(model,color,feature,price){
-//         super(model,color); // Invoking the parent Constructor
-//         this.feature = feature;
-//         this.price = price;
-//     }
-// }
-
-// let s1 = new SuperCar("BMW","Red",["f1","f2"],100000000);
-
-// let s2 = new SuperCar("Nissan","Blue",["f1","f2"],100000000);
-
-// console.log(s1);
-
-// console.log(s2);
-
-// let val = s1.getModel();
-
-// console.log(val);
 
 
 
@@ -83,27 +34,6 @@ console.log(obj2);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// How will s1 look like :-
-// {model:"bmw"; color:"red"; feature:[f1,f2]; price:10000}
-// s1 is built from SuperCar. Therefore, in the prototype of s1 we will store the constructor -> Class SuperCar.
-// Immediate parent of SuperCar is Car. So in the prototype of SuperCar we will store constructor -> Class Car and getModel.
-// Parent of Car is Native JS Object. So in the prototype of Car we will store the Native JS Object's functionalities
 
 
 
