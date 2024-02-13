8f37e491c654508df18907e5b5df86e34d4841a0
Subhadip Das <subhadipdas911@gmail.com>
2024-02-13 18:25:09 +0530

    Explained as to how we are reaching to the output in Example Number 2

--- Javascript/OOPS-3/Chirag/Memory.js
+++ Javascript/OOPS-3/Chirag/Memory.js
@@ -1,3 +1,5 @@
+// * Example-01
+
 var a = 10;
 var b = 20;
 var c = b;
@@ -9,3 +11,16 @@ console.log(a,b,c);
 // The values that we are storing inside the variables will depend on what we are trying to store.
 // If we are storing Primitive DT. Then those values will be stored in the Callstack itself.
 // * Output will be 10, 30, 20
+
+// * Example-02
+
+var a = 10;
+var b = 20;
+
+a = b;
+
+console.log(a,b);
+// Variables are always stored inside the Callstack.
+// The values that we are storing inside the variables will depend on what we are trying to store.
+// If we are storing Primitive DT. Then those values will be stored in the Callstack itself.
+// * Output will be 20,20
