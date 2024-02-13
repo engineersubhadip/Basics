f69276f66937ad7a22b442c1c07a04a5103d7495
Subhadip Das <subhadipdas911@gmail.com>
2024-02-13 18:55:27 +0530

    Explained how the memory reference of the Heap Memory is being shared to the Callstack and how values are being updated

--- Javascript/OOPS-3/Chirag/Memory.js
+++ Javascript/OOPS-3/Chirag/Memory.js
@@ -43,3 +43,25 @@ console.log(a,b)
 // The values that we are storing inside the variables will depend on what we are trying to store.
 // If we are storing Primitive DT. Then those values will be stored in the Callstack itself.
 // * Output will be 10,10
+
+// *Example-04
+
+var a = {name:"Shahbaz"};
+
+var b = {name:"Poorna"};
+
+var c = b;
+c.name = "Nirmal";
+
+console.log(a,b,c);
+// Variables are stored inside the Callstack.
+// The values that we are storing inside the variables will depend on what we are trying to store.
+// Here we are storing Reference DT. So the values will be stored in the Heap Memory and the memory address will be shared to the variables inside the Callstack.
+// Meaning the variables from the Callstack are pointing to the Heap Memory where the objects are being stored.
+
+// a -> {name:"Shahbaz"}
+// b -> {name:"Poorna"}
+// c also points to the same object as b is pointing to.
+// c.name = Nirmal -> Meaning whatever "b" and "c" were pointing to gets updated to {name:"Nirmal"}
+
+// a -> {name:"Shahbaz"} ; b -> {name:"Nirmal"} ; c -> {name:"Nirmal"}
