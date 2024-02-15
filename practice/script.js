4b00bc354af370ab99d077a55cdfc1abd9d0da06
Subhadip Das <subhadipdas911@gmail.com>
2024-02-15 22:25:17 +0530

    Implemented Reduce to address Reduce

--- practice/script.js
+++ practice/script.js
@@ -1,28 +1,50 @@
-let array = [12,13,14,15,15,16];
+const data = [{
+    id: 1,
+    type: "CREDIT",
+    amount: 1000
+    }, 
+    {
+    id: 1,
+    type: "DEBIT",
+    amount: 5000
+    }, 
+    {
+    id: 1,
+    type: "CREDIT",
+    amount: 1000
+    }, 
+    {
+    id: 1,
+    type: "CREDIT",
+    amount: 10000
+    }, 
+    {
+    id: 1,
+    type: "DEBIT",
+    amount: 2000
+    }];
 
-// let result = array.filter(function(ele){
-//     if (ele % 2 == 0){
-//         return true
-//     }
-// })
+let res1 = data.reduce(function(acc1,curr1){
+    let currType = curr1.type.toLowerCase();
+    let currAmount = curr1.amount;
 
-// console.log(result);
-
-Array.prototype.customFilter = function(callback){
-    let result = [];
-    for (let i=0; i<this.length; i++){
-        let currVal = callback(this[i]);
-        if (currVal){
-            result.push(this[i]);
-        }
+    if (acc1[currType] === undefined){
+        acc1[currType] = [currAmount];
+    }else{
+        acc1[currType].push(currAmount);
     }
-    return result;
-}
+    return acc1;
+},{});
 
-let val = array.customFilter(function(ele){
-    if (ele % 2 == 0){
-        return true
-    }
-});
 
-console.log(val);
+let res2 = Object.keys(res1).reduce(function(acc2,curr2){
+    let currType = curr2;
+    let currArray = res1[currType].reduce(function(acc3,curr3){
+        return acc3+curr3;
+    },0);
+    acc2[currType] = currArray;
+    return acc2;
+},{});
+
+console.log(res2);
+
