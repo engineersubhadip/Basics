5ba2859cbd7f5e109b55ac33ca2c65383045478c
Subhadip Das <subhadipdas911@gmail.com>
2024-02-16 23:07:41 +0530

    Rectified the explanation points

--- Javascript/Callbacks/Chirag/Class2/script2.js
+++ Javascript/Callbacks/Chirag/Class2/script2.js
@@ -35,12 +35,12 @@ fetchUserData("www.dropbox.in",function f1(error1,data1){  // Once we have fetch
         console.log("The error from fetching user data is",error1);
     }else{
         console.log("The data recovered is",data1);
-        fetchPostData(data1,function f2(error2,data2){ // Once we have fetched the posts from the userdata, then f2 will be executed. f2 is a callback.
+        fetchPostData(data1,function f2(error2,data2){ // Once we have fetched the posts from the data1, then f2 will be executed. f2 is a callback.
             if(error2){
                 console.log("The error from fetching the posts is",error2);
             }else{
                 console.log("The user posts details are",data2);
-                fetchCommentData(data2,function f3(error3,data3){ // Once we have fetched the comments from the userPosts, then f3 will be executed, f3 is a callback
+                fetchCommentData(data2,function f3(error3,data3){ // Once we have fetched the comments from the data2, then f3 will be executed, f3 is a callback
                     if(error3){
                         console.log("The error from fetching the comments is",error3);
                     }else{
