20d6e0075c833ba752c07a1af87936c48d569714
Subhadip Das <subhadipdas911@gmail.com>
2024-02-16 12:10:17 +0530

    Implemented the Scenario via setInterval

--- Javascript/Callbacks/Chirag/example2.js
+++ Javascript/Callbacks/Chirag/example2.js
@@ -1,2 +1,17 @@
 // * Create a animation function "continousAnimation" that continuously changes the background color of an element between red and blue.
 // * The function should take the element's ID as an argument and update the background color every 500ms.
+
+let elementID = document.querySelector("#container");
+
+function continousAnimation(elementID){
+    let currColor = elementID.style.backgroundColor;
+    if (currColor === "red"){
+        elementID.style.backgroundColor = "blue";
+    }else{
+        elementID.style.backgroundColor = "red";
+    }
+}
+
+setInterval(function(){
+    continousAnimation(elementID);
+},500);
