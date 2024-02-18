fc17113e51e6833a4165f53d8c065464fe85acc8
Subhadip Das <subhadipdas911@gmail.com>
2024-02-18 20:44:23 +0530

    Successfully implemented the scenario where the user can pass any dynamic record inside the API

--- Javascript/Promises/Chirag/example4.js
+++ Javascript/Promises/Chirag/example4.js
@@ -1,4 +1,4 @@
-// Build a custom Promise similar to fetch that will accept a userURl and  return the stock after 10s.
+// Build a custom Promise similar to fetch that will accept data as parameter and  return the stock after 10s.
 // If not present will return a error message 
 
 let data = {
@@ -20,3 +20,45 @@ let data = {
       "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
     ],
   };
+
+
+let customFetch = function(userRecord){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            if (userRecord === undefined){
+                reject("Please pass a parameter inside customFetch");
+                return 
+            }
+            if (userRecord.stock){
+                resolve(userRecord.stock);
+            }else{
+                reject("Sorry the key does not exist")
+            }
+        },10000);
+    });
+};
+
+customFetch({
+    id: 1,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    thumbnail: "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+    images: [
+      "https://cdn.dummyjson.com/product-images/1/1.jpg",
+      "https://cdn.dummyjson.com/product-images/1/2.jpg",
+      "https://cdn.dummyjson.com/product-images/1/3.jpg",
+      "https://cdn.dummyjson.com/product-images/1/4.jpg",
+      "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+    ],
+  }).then(function(data){
+    console.log(data);
+})
+.catch(function(error){
+    console.log(error);
+})
