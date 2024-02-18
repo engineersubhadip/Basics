1c2665989c10c1d9a254f8e2873a32340f6850be
Subhadip Das <subhadipdas911@gmail.com>
2024-02-18 20:36:09 +0530

    Initialied with the Problem statement

---
+++ Javascript/Promises/Chirag/example4.js
@@ -0,0 +1,22 @@
+// Build a custom Promise similar to fetch that will accept a userURl and  return the stock after 10s.
+// If not present will return a error message 
+
+let data = {
+    id: 1,
+    title: "iPhone 9",
+    description: "An apple mobile which is nothing like apple",
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: "Apple",
+    category: "smartphones",
+    thumbnail: "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+    images: [
+      "https://cdn.dummyjson.com/product-images/1/1.jpg",
+      "https://cdn.dummyjson.com/product-images/1/2.jpg",
+      "https://cdn.dummyjson.com/product-images/1/3.jpg",
+      "https://cdn.dummyjson.com/product-images/1/4.jpg",
+      "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
+    ],
+  };
