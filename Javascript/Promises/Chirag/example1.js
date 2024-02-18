dd03b46578523bb825585ae1fe074067f9aa8fad
Subhadip Das <subhadipdas911@gmail.com>
2024-02-18 14:15:05 +0530

    Explained how .then and .catch works

--- Javascript/Promises/Chirag/example1.js
+++ Javascript/Promises/Chirag/example1.js
@@ -12,3 +12,13 @@ let x = new Promise(function(resolve,reject){
 console.log(x);
 
 // x will be a promise object whose state will be FULFILLED and value as "You have passed"
+
+x.then(function f1(data){
+    console.log(data);
+})
+.catch(function f2(err){
+    console.log(err);
+});
+
+// .then will be executed ONCE THE PROMISE resolves
+// .catch will be executed ONCE THE PROMISE rejects
