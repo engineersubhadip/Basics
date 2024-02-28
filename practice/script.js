056b8067683fe32441101149be8e3b62a4f2152c
Subhadip Das <subhadipdas911@gmail.com>
2024-02-28 21:53:35 +0530

    Race between Macro and Micro Task Queue Execution

--- practice/script.js
+++ practice/script.js
@@ -1,50 +1,16 @@
-const data = [{
-    id: 1,
-    type: "CREDIT",
-    amount: 1000
-    }, 
-    {
-    id: 1,
-    type: "DEBIT",
-    amount: 5000
-    }, 
-    {
-    id: 1,
-    type: "CREDIT",
-    amount: 1000
-    }, 
-    {
-    id: 1,
-    type: "CREDIT",
-    amount: 10000
-    }, 
-    {
-    id: 1,
-    type: "DEBIT",
-    amount: 2000
-    }];
-
-let res1 = data.reduce(function(acc1,curr1){
-    let currType = curr1.type.toLowerCase();
-    let currAmount = curr1.amount;
-
-    if (acc1[currType] === undefined){
-        acc1[currType] = [currAmount];
-    }else{
-        acc1[currType].push(currAmount);
-    }
-    return acc1;
-},{});
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve("We will be printing second");
+    },10000);
+})
+.then(function(data){
+    console.log("The data from the Promise Object is ->",data);
+})
+.catch(function(error){
+    console.log("The error from the Promise Object is ->",error);
+})
 
 
-let res2 = Object.keys(res1).reduce(function(acc2,curr2){
-    let currType = curr2;
-    let currArray = res1[currType].reduce(function(acc3,curr3){
-        return acc3+curr3;
-    },0);
-    acc2[currType] = currArray;
-    return acc2;
-},{});
-
-console.log(res2);
-
+setTimeout(function(){
+    console.log("I am going to get printed first");
+});
