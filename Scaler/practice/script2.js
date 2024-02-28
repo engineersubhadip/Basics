0289c74f5ef0461d79bafd640102c5c58bda60a4
Subhadip Das <subhadipdas911@gmail.com>
2024-02-28 14:40:47 +0530

    Created a custom Function performing async operation of countdown

---
+++ Scaler/practice/script2.js
@@ -0,0 +1,14 @@
+let timerID;
+
+function countDownWithTimer(userInput){
+    let target = userInput - 5;
+    timerID = setInterval(function(){
+        if(userInput == target){
+            clearInterval(timerID);
+        }else{
+            console.log(userInput);
+            userInput -= 1;
+        }
+    },1000);
+}
+countDownWithTimer(12);
