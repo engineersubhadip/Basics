938ba7c0251abd52ac88b589ea15fcda099eefd9
Subhadip Das <subhadipdas911@gmail.com>
2024-03-01 00:56:03 +0530

    Implemented the Problem statement

---
+++ Async-Await/example2.js
@@ -0,0 +1,3 @@
+// Imagine you're developing a real-time dashboard for monitoring stocks. You need to fetch the latest price data for multiple stocks from different sources and display them on the dashboard. How would you use promises to ensure that all price data is fetched efficiently and handle any errors that might occur during the process?
+
+let stockNames = ["A K Spintex","Aakar Engg & Mf","Aarnav Fashions","Aasheesh Sec"];
