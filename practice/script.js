9082ec841b8467dd206fe923030e137aee290e9c
Subhadip Das <subhadipdas911@gmail.com>
2024-03-01 07:40:45 +0530

    Two Await Syntaxes

--- practice/script.js
+++ practice/script.js
@@ -1,10 +1,13 @@
 async function fun(){
     let data = await Promise.resolve("Here is the data");
     console.log(data);
+
+    let data2 = await Promise.resolve("Here is the second One");
+    console.log(data2);
 }
 
 fun();
 
-console.log(fun());
+console.log(fun()); // without return and exception we always get fulfilled Promise
 
 console.log("To be printed second");
