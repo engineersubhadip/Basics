025093d727bafac26353bade8633d3e7de9293e8
Subhadip Das <subhadipdas911@gmail.com>
2024-03-02 13:49:29 +0530

    Initial Commit of the File

---
+++ Javascript/Polyfills/Bind/script.js
@@ -0,0 +1,8 @@
+function getDetails(color,speed){
+    console.log(`I bought ${this.name} car at ${this.price} of color ${color} and speed is ${speed}`);
+}
+
+let obj1 = {
+    name:"Mercedes",
+    price:1000000
+}
