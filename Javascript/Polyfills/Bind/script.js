21749d4abc17632a20addb74c23d55671492bd63
Subhadip Das <subhadipdas911@gmail.com>
2024-03-02 14:04:12 +0530

    PolyFill of Bind

--- Javascript/Polyfills/Bind/script.js
+++ Javascript/Polyfills/Bind/script.js
@@ -1,8 +1,26 @@
 function getDetails(color,speed){
-    console.log(`I bought ${this.name} car at ${this.price} of color ${color} and speed is ${speed}`);
+    return `I bought ${this.name} car at ${this.price} of color ${color} and speed is ${speed}`;
 }
 
 let obj1 = {
     name:"Mercedes",
     price:1000000
 }
+
+
+// let res = getDetails.bind(obj1);
+
+// res("blue",1222);
+
+Function.prototype.customBind = function(obj,...args1){
+    obj.fxn = this; // {name:Mercedes, price:100000, fxn:Function}
+    return function f1(...args2){
+        return obj.fxn(...args1,...args2);
+    }
+}
+
+let res = getDetails.customBind(obj1,"Yellow",999);
+
+console.log(res);
+
+console.log(res())
