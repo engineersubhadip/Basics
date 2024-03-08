9be3999f696372808d77dd33d6e5c868a21c3c2d
Subhadip Das <subhadipdas911@gmail.com>
2024-03-08 21:27:47 +0530

    We are using bind to set the context of person4 object

--- Javascript/OOPS-1/ExampleSet-2/script.js
+++ Javascript/OOPS-1/ExampleSet-2/script.js
@@ -19,6 +19,11 @@ let person3 = {
     age:49
 }
 
+let person4 = {
+    name:"Rourke",
+    age:55
+}
+
 person1.setMovies("Batman","Hypnotic","Argo");
 
 // * We shall be using the context of setMovies to set the properties of person2.
@@ -29,3 +34,12 @@ person1.setMovies.call(person2,"Two and a half men","Gentlemen","Figure of Speec
 
 person1.setMovies.apply(person3,["CatWoman","SpiderMan","Captain Marvel"]);
 
+// * We shall be using the context of setMovies to set the properties of person3.
+
+let setMoviesPerson4 = person1.setMovies.bind(person4);
+
+console.log(setMoviesPerson4); // This will be a function, which takes in 3 arguments
+
+setMoviesPerson4("Agent-One","33 and Counting","Mission One");
+
+console.log(person4);
