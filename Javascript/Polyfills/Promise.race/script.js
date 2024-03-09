89885300b0af58afb5ebb0b2c415a5072c05d704
Subhadip Das <subhadipdas911@gmail.com>
2024-03-09 19:35:12 +0530

    Implemented the Polyfill of Promise.race

--- Javascript/Polyfills/Promise.race/script.js
+++ Javascript/Polyfills/Promise.race/script.js
@@ -8,7 +8,7 @@ let p1 = new Promise(function(resolve,reject){
 let p2 = new Promise(function(resolve,reject){
     setTimeout(function(){
         reject("P2 is getting resolved in 3s");
-    },3000);
+    },6000);
 });
 
 let p3 = new Promise(function(resolve,reject){
@@ -17,15 +17,36 @@ let p3 = new Promise(function(resolve,reject){
     },5000);
 });
 
-Promise.race([p1,p2,p3])
-.then(function(data){
-    console.log(data);
-})
-.catch(function(error){
-    console.log("Error message is ->",error);
-})
+// Promise.race([p1,p2,p3])
+// .then(function(data){
+//     console.log(data);
+// })
+// .catch(function(error){
+//     console.log("Error message is ->",error);
+// })
 
 // * Architecture :- Promise.race takes in array of Promises. Whichever Promises settles ->(fulfills/rejects) first, the value of that promise object will be shown
 // * It returns a Promise Object with the promise value being the first settled Promise.
 
+Promise.customPromiseRace = function(promiseArray){
+    return new Promise(function(resolve,reject){
+        for (let i=0; i<promiseArray.length; i++){  // The idea is to iterate through the list of the Promises and whichever Promise settles first, we will either resolve/reject the entire Promise Object (returned from Promise.race) based on that
+            promiseArray[i]
+            .then(function(data){
+                resolve(data);
+            })
+            .catch(function(error){
+                reject(error);
+            })
+        }
+    });
+};
 
+
+Promise.customPromiseRace([p1,p2,p3])
+.then(function(data){
+    console.log("Resolved Value is",data);
+})
+.catch(function(error){
+    console.log("Error Message is",error);
+})
