38a6f706c3ad1606a13b47a588deb0e4b585a82e
Subhadip Das <subhadipdas911@gmail.com>
2024-03-09 20:30:53 +0530

    Optimized the PolyFill of Bind

--- Javascript/Polyfills/Bind/script.js
+++ Javascript/Polyfills/Bind/script.js
@@ -10,17 +10,26 @@ let obj1 = {
 
 // let res = getDetails.bind(obj1);
 
+// console.log(obj1);
+
 // res("blue",1222);
+
+// console.log(obj1);
 
 Function.prototype.customBind = function(obj,...args1){
     obj.fxn = this; // {name:Mercedes, price:100000, fxn:Function}
     return function f1(...args2){
-        return obj.fxn(...args1,...args2);
+        let result = obj.fxn(...args1,...args2);
+        delete obj.fxn;
+        return result;
     }
 }
 
 let res = getDetails.customBind(obj1,"Yellow",999);
 
+console.log(obj1);
 console.log(res);
 
 console.log(res())
+
+console.log(obj1);
