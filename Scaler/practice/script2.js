51ac90bb87214f6cb9c229ca5b2a37791566a031
Subhadip Das <subhadipdas911@gmail.com>
2024-03-11 22:20:47 +0530

    Simulated the Coffee Shop Problem via the usage of Promise Chaining

--- Scaler/practice/script2.js
+++ Scaler/practice/script2.js
@@ -1,12 +1,49 @@
-function outer(){
-    var x = 10;
-    function inner(){
-        console.log(x);
-        var x = 20;
-    }
-    return inner;
-}
+// Coffee Shop Problem:-
 
-var closureFunc = outer();
+function placeOrder(userOrder){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            if (userOrder === "coffee"){
+                resolve("We have accepted your order");
+            }else{
+                reject("We do not serve this particular order",userOrder);
+            }
+        },3000);
+    });
+};
 
-closureFunc();
+function processOrder(){
+    return new Promise(function(resolve,reject){
+        console.log("We are now processing your order...");
+        setTimeout(function(){
+            resolve("Order has been processed")
+        },4000);
+    });
+};
+
+
+function generateBill(userOrder){
+    return new Promise(function(resolve,reject){
+        setTimeout(function(){
+            let amount = 13.22;
+            resolve(amount);
+        },3000);
+    });
+};
+
+placeOrder("coffee")
+.then(function(data){
+    console.log(data);
+    return processOrder();
+})
+.then(function(data){
+    console.log(data);
+    return generateBill("coffee");
+})
+.then(function(data){
+    console.log("Payable amount is",data);
+    console.log("Have a good day");
+})
+.catch(function(error){
+    console.log("Error is ->",error);
+})
